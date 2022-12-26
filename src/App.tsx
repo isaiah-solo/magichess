9ac0f6bde79f9eebcd9eaa788f923927f783cd6c
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2022-12-26 01:08:19 -0800

    added control panel

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Board from './ui/game/Board';
 import {createGameStoreWithDefaultedBoard} from './state/game/gameStore';
 import deserializeBoard from './utils/deserializeBoard';
 import {CHESS_DEFAULT_PIECES} from './utils/devInitBoards';
+import ControlPanel from './ui/game/ControlPanel';
 
 export default function App() {
   const defaultedGameStore = createGameStoreWithDefaultedBoard(
@@ -10,16 +11,33 @@ export default function App() {
   );
 
   return (
-    <div
-      style={{
-        height: '100vh',
-        overflowY: 'auto',
-        position: 'absolute',
-        width: '100vw',
-      }}>
-      <Provider store={defaultedGameStore}>
-        <Board />
-      </Provider>
-    </div>
+    <Provider store={defaultedGameStore}>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr auto',
+          height: '100vh',
+          overflowY: 'auto',
+          position: 'absolute',
+          width: '100vw',
+        }}>
+        <div
+          style={{
+            height: '100%',
+            position: 'relative',
+            width: '100%',
+          }}>
+          <Board />
+        </div>
+        <div
+          style={{
+            borderLeft: '1px solid black',
+            height: '100%',
+            width: 400,
+          }}>
+          <ControlPanel />
+        </div>
+      </div>
+    </Provider>
   );
 }
---
+++ src/ui/game/ControlPanel.tsx
@@ -0,0 +1,45 @@
+import {useBoardActions, useSelectSlots} from '../../state/game/boardSlice';
+import {
+  useCheckAndAssumeWinnerEffect,
+  useFinishTurn,
+  useSelectCurrentTurn,
+  useSelectWinner,
+} from '../../state/game/gameSlice';
+import Team from '../../models/pieces/Team';
+
+const COLOR_MAP = {
+  [Team.One]: '#0081a7',
+  [Team.Two]: '#f07167',
+};
+
+export default function ControlPanel() {
+  const currentTurn = useSelectCurrentTurn();
+  const winner = useSelectWinner();
+
+  useCheckAndAssumeWinnerEffect();
+
+  return (
+    <div
+      style={{
+        alignItems: 'center',
+        boxSizing: 'border-box',
+        display: 'grid',
+        gridGap: 16,
+        gridTemplateColumns: '1fr',
+        height: 'fit-content',
+        padding: 16,
+        width: '100%',
+      }}>
+      <div
+        style={{
+          backgroundColor: COLOR_MAP[winner ?? currentTurn],
+          boxSizing: 'border-box',
+          height: 60,
+          padding: 16,
+          width: '100%',
+        }}>
+        {winner !== null ? 'Winner!' : 'Current Turn'}
+      </div>
+    </div>
+  );
+}
