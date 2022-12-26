aa7eb2aae4d73074bcc2b8b9eed1f6a2f564741a
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2022-12-26 01:38:32 -0800

    added styling to move dot

--- src/ui/game/PossibleMoveDot.tsx
+++ src/ui/game/PossibleMoveDot.tsx
@@ -1,19 +1,18 @@
+import styled from 'styled-components';
+
 type Props = {
   onClick?: () => void;
 };
 
+const StyledPossibleMoveDotDiv = styled.div`
+  background-color: #f5e0b7;
+  border: 1px solid #191919;
+  border-radius: 50%;
+  cursor: pointer;
+  height: 40px;
+  width: 40px;
+`;
+
 export default function PossibleMoveDot({onClick}: Props) {
-  return (
-    <div
-      onClick={onClick}
-      style={{
-        backgroundColor: '#f5e0b7',
-        border: '1px solid #191919',
-        borderRadius: '50%',
-        cursor: 'pointer',
-        height: 40,
-        width: 40,
-      }}
-    />
-  );
+  return <StyledPossibleMoveDotDiv onClick={onClick} />;
 }
