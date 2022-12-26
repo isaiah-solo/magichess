95c1bf7e98dcb9a1ce86affbe45fde23185d21c4
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2022-12-26 01:40:21 -0800

    added styling to tile content

--- src/ui/game/PossibleMoveDot.tsx
+++ src/ui/game/PossibleMoveDot.tsx
@@ -1,9 +1,5 @@
 import styled from 'styled-components';
 
-type Props = {
-  onClick?: () => void;
-};
-
 const StyledPossibleMoveDotDiv = styled.div`
   background-color: #f5e0b7;
   border: 1px solid #191919;
@@ -12,6 +8,10 @@ const StyledPossibleMoveDotDiv = styled.div`
   height: 40px;
   width: 40px;
 `;
+
+type Props = {
+  onClick?: () => void;
+};
 
 export default function PossibleMoveDot({onClick}: Props) {
   return <StyledPossibleMoveDotDiv onClick={onClick} />;
--- src/ui/game/TileContent.tsx
+++ src/ui/game/TileContent.tsx
@@ -1,20 +1,18 @@
 import {ReactNode} from 'react';
+import styled from 'styled-components';
+
+const StyledTileContentDiv = styled.div`
+  left: 50%;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  width: fit-content;
+`;
 
 type Props = {
   children?: ReactNode | null;
 };
 
 export default function TileContent({children = null}: Props) {
-  return (
-    <div
-      style={{
-        left: '50%',
-        position: 'relative',
-        top: '50%',
-        transform: 'translateY(-50%) translateX(-50%)',
-        width: 'fit-content',
-      }}>
-      {children}
-    </div>
-  );
+  return <StyledTileContentDiv>{children}</StyledTileContentDiv>;
 }
