0cde0108b88a772bc353f93f9a898d35e54782a1
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2022-12-26 01:30:22 -0800

    added styling to pieces

--- src/ui/game/Piece.tsx
+++ src/ui/game/Piece.tsx
@@ -1,11 +1,41 @@
-import {ReactNode} from 'react';
+import styled, {css} from 'styled-components';
 import PieceModel from '../../models/pieces/Piece';
 import Team from '../../models/pieces/Team';
 
-const TEAM_COLOR_MAP = {
-  [Team.One]: '#0081a7',
-  [Team.Two]: '#f07167',
-};
+const StyledPieceDiv = styled.div`
+  border: 1px solid #191919;
+  border-radius: 50%;
+  cursor: pointer;
+  height: 60px;
+  width: 60px;
+
+  ${({isHighlighted, team}: {isHighlighted: boolean; team: Team}) => {
+    if (isHighlighted) {
+      return css`
+        background-color: #f5e0b7;
+      `;
+    }
+
+    switch (team) {
+      case Team.One:
+        return css`
+          background-color: #0081a7;
+        `;
+      case Team.Two:
+        return css`
+          background-color: #f07167;
+        `;
+    }
+  }}
+`;
+
+const StyledPieceContentDiv = styled.div`
+  left: 50%;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  width: fit-content;
+`;
 
 type Props = {
   isHighlighted?: boolean;
@@ -15,28 +45,11 @@ type Props = {
 
 export default function Piece({isHighlighted = false, onClick, piece}: Props) {
   return (
-    <div
+    <StyledPieceDiv
+      isHighlighted={isHighlighted}
       onClick={onClick}
-      style={{
-        backgroundColor: !isHighlighted
-          ? TEAM_COLOR_MAP[piece.getTeam()]
-          : '#f5e0b7',
-        border: '1px solid #191919',
-        borderRadius: '50%',
-        cursor: 'pointer',
-        height: 60,
-        width: 60,
-      }}>
-      <div
-        style={{
-          left: '50%',
-          position: 'relative',
-          top: '50%',
-          transform: 'translateY(-50%) translateX(-50%)',
-          width: 'fit-content',
-        }}>
-        {piece.getName()}
-      </div>
-    </div>
+      team={piece.getTeam()}>
+      <StyledPieceContentDiv>{piece.getName()}</StyledPieceContentDiv>
+    </StyledPieceDiv>
   );
 }
