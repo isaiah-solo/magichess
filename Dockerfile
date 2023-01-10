19307e901f5a0c720ad1c1cab5b5089e15fffd76
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2023-01-09 17:30:11 -0800

    swapped travis with Dockerhub

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:carbon as builder
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+
+FROM nginx
+COPY --from=builder /usr/src/app/build/ /usr/share/nginx/html
+COPY --from=builder /usr/src/app/default.conf /etc/nginx/conf.d/
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- docker_deploy.sh
+++
@@ -1,7 +0,0 @@
-echo "$DOCKER_PASS" | docker login -u $DOCKER_USER --password-stdin
-export REPO=isaiahsolo/personal-site-frontend
-export TAG=`if [ "$TRAVIS_BRANCH" == "main" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-docker build -f Dockerfile -t $REPO:$COMMIT .
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
-docker push $REPO
